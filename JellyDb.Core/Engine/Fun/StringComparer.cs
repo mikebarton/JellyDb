2f9ac25306d36830a8f32aa1a6889981202fb465
Mike Barton <mike__barton@hotmail.com>
2015-06-21 22:12:04 +1000

    work on string comparer

--- JellyDb.Core/Engine/Fun/StringComparer.cs
+++ JellyDb.Core/Engine/Fun/StringComparer.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace JellyDb.Core.Engine.Fun
 {
     public class StringComparer : ITypeComparer<string>
     {
+        private const string _allowedCharsRegex = @"^[a-zA-Z\-.]{0,32}$";
+
         public string MinKey
         {
-            get { throw new NotImplementedException(); }
+            get { return string.Empty; }
         }
 
         public string MaxKey
@@ -19,17 +22,27 @@ namespace JellyDb.Core.Engine.Fun
 
         public int Compare(string one, string two)
         {
-            throw new NotImplementedException();
+            if (one == null || two == null) throw new ArgumentNullException("one/two");
+            return String.Compare(one, two);
         }
 
         public string Decrement(string input)
         {
-            throw new NotImplementedException();
+            BitConverter.(BitConverter.GetBytes(Int32.MaxValue), 0);
         }
 
         public string Increment(string input)
         {
             throw new NotImplementedException();
+        }
+
+        private bool IncrementChar(char input, out char c)
+        {
+            input++;
+            if (Regex.IsMatch(new string(new[] { input }), _allowedCharsRegex))
+            {
+
+            }
         }
     }
 }
